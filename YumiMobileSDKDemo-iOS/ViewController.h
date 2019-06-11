dece0d4f412fb3c3c784a752bd700485b7ff9135
ShunZhi Tang <tangshunzhi@zplay.com>
2019-06-11 10:58:10 +0800

    Initial Commit

---
+++ YumiMobileSDKDemo-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YumiMobileSDKDemo-iOS
+//
+//  Created by Michael Tang on 2019/6/11.
+//  Copyright © 2019 MichaelTang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
